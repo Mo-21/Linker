f4a2eadd9150f7244629c06e7e604c7e03ad9e33
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-04-06 20:46:20 +0300

    add tooltip on click

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,12 @@ links.forEach((link) => {
     event.preventDefault();
     const textToCopy = link.querySelector(".hyperlink")?.textContent;
     if (textToCopy) navigator.clipboard.writeText(textToCopy);
+
+    const copyAlert = link.querySelector(".hide-alert");
+    copyAlert?.classList.add("show-alert");
+
+    setTimeout(() => {
+      copyAlert?.classList.remove("show-alert");
+    }, 2000);
   });
 });
