33b7ea6b59973e31993e7357774c72964438e65e
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-04-06 18:59:55 +0300

    copy link to clipboard

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import "./style.css";
+
+const links = document.querySelectorAll(".link");
+
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const textToCopy = link.querySelector(".hyperlink")?.textContent;
+    if (textToCopy) navigator.clipboard.writeText(textToCopy);
+  });
+});
